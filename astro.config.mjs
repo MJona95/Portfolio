60e044aeeb102ef151e8267ed1d7ff3a3a85af69
Marvin Gutierrez <mjona9514@gmail.com>
2025-01-31 21:38:39 -0600

    deploy configuration remove unused module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Mjona95.github.io",
   base: 'Portfolio',
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
-  i18n:{
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing:{
-      prefixDefaultLocale: true,
-      //redirectToDefaultLocale: true
-    }
-  }
+  integrations: [],
+
 });
