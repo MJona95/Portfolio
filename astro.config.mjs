c1811b5741bede6d5cec176397066dfd84a73a85
Marvin Gutierrez <mjona9514@gmail.com>
2025-01-31 21:32:33 -0600

    deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://Mjona95.github.io",
+  base: 'Portfolio',
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  i18n:{
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing:{
+      prefixDefaultLocale: true,
+      //redirectToDefaultLocale: true
+    }
+  }
+});
