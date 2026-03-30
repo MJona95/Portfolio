d6742f7ca229e014596015a658f59bd2d45afb40
Marvin Gutierrez <mjona9514@gmail.com>
2026-03-30 08:33:36 -0600

    Config actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,19 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://mjonaf95.github.io',
-  base: '/',  // Para página de usuario, es '/'
+  base: '/',  // Para página de usuario
   outDir: './dist',
   build: {
-    format: 'directory'
+    format: 'directory',
+    // Esto es importante para rutas dinámicas
+    redirects: false
+  },
+  // Configuración para i18n
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: true
+    }
   }
 });
